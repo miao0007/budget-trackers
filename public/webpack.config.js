1346a076274ae6cead2b2dac4432b29193732e79
miao0007 <miao188@hotmail.com>
2020-09-19 17:10:34 +0930

    added webpack.comfig.js

---
+++ public/webpack.config.js
@@ -0,0 +1,48 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+
+const config = {
+  entry: {
+    app: './index.js',
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      name: 'PWA Online/Offline Budget Tracker',
+      short_name: 'Budget Tracker',
+      description: 'An application that allows you to add expenses and deposits to their budget with or without a connection. When entering transactions offline, they should populate the total when brought back online.',
+      background_color: '#01579b',
+      theme_color: '#ffffff',
+      'theme-color': '#ffffff',
+      start_url: '/',
+      icons: [
+        {
+          src: path.resolve('./icons/icon-192x192.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
+  ],
+};
+
+module.exports = config;
